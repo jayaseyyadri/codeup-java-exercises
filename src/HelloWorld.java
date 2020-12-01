f61f0a4bec77dfd729e5b2e8d82cb1fc8b5768cf
jaya <jayaseyyadri@gmail.com>
2020-12-01 15:25:28 -0600

    first exercise

---
+++ src/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.print("Hello world, from the command line");
+
+    }
+}
