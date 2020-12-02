36299ac210bb6942d071a9bc83c44fa039f1f511
jaya <jayaseyyadri@gmail.com>
2020-12-02 11:22:53 -0600

    completed exercise 2 in Java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,8 +1,63 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
-        System.out.print("Hello world, from the command line");
+//        System.out.println("Hello world");
+//        System.out.print("Hello world, from the command line");
+  int myFavoriteNumber=6;
+        System.out.println(myFavoriteNumber);
+        String myString="jaya";
+//       myString ='j';->incompatible types
+//       myString = 3.14159;->incompatible types
+        System.out.println(myString);
+
+//        float myNumber = 3.14;
+//        float myNumber = (float)3.14;
+//
+        double myNumber=3.14;
+//         myNumber=(float)3.14;
+
+
+        System.out.println(myNumber);
+
+//        -->variable not initialized
+//      long myNumber =3.14  possible lossy conversion from double to long
+
+//        int x = 5;
+
+//                prints value first increments later
+//        System.out.println(x++);
+//        System.out.println(x);
+
+
+//        System.out.println(++x);
+//        System.out.println(x);
+        //        increments immediately
+
+//        class--> reserved word
+
+//
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+//        string to an integer
+
+//        int three = (int) "three";
+
+//        int x = 4;
+//        x = x + 5;
+//           =>x+=5;
+
+
+//        int x = 3;
+//        int y = 4;
+//          => y *= x;
+//
+//        int x = 10;
+//        int y = 2;
+//         x /= y;
+//         y -=  x;
+//
+//        System.out.println(y);
 
     }
 }
---
+++ src/Syntax.java
@@ -0,0 +1,173 @@
+
+    public class Syntax {
+
+        public static void main(String[] args) {
+
+
+            // ================== STATEMENTS (s-3)
+
+            // Statements may be...
+            // Declarations
+//                 int x;
+
+            // Assignment expressions
+//                 x = 3;
+
+            // Any use of ++ or --
+//                 x++;
+//                 --x;
+
+            // Method invocations
+//                 Math.random();
+
+            // Object creation expressions
+//                 SomeObject = new SomeObject();
+
+            // ================== COMMENTS (s-4)
+
+            // single line comment
+
+        /*
+
+            multi-line comment
+
+         */
+
+            /**
+             * Comment block
+             */
+
+//        System.out.println("test");
+
+
+            // ================== DATA TYPES (s-5, 6, 7)
+
+
+
+//            int primitiveExample = 3;
+//            System.out.println(primitiveExample);
+
+
+//            Student referenceExample = new Student();
+
+
+            // use the smallest type needed for the job
+
+
+            // primitive types...
+
+//            byte age = -32;
+//        System.out.println(age);
+//            short minimumAnnualSalary = 15080;
+//            int distanceToSaturn = 1200000000;
+//            long numberOfHumanCells = 3000000000L; // L needed for compiler
+//            System.out.println(numberOfHumanCells);
+//            float lengthOfBacteriaInInches = 0.00004F; // F needed for compiler
+//            System.out.println(lengthOfBacteriaInInches);
+//            double widthOfAtomInMeters = .00000000001;
+//            char seatingSection = 'M'; // must wrap in single quotes
+//            boolean everythingIsAwesome = false;
+
+            // strings (not a primitive type)
+
+//                 String thisIsAString = "Hello";
+
+
+
+//              like in JS, strings must escape certain characters like quotes and use it to inject newline characters and other formatting
+
+//        System.out.println("Hello\nWorld");
+//        System.out.print("Hello");
+//        System.out.print("World");
+
+//        System.out.println("He said \"Hi\"");
+//
+//        String hello = "Hello";
+//        char someLetter = hello.charAt(0);
+//        System.out.println(someLetter);
+
+
+            // ================== VARIABLES (s-8)
+
+            // declaration separate from initialization
+
+//             int seven;
+//             seven = 7;
+//            System.out.println(seven);
+
+            // declaration and initialization
+
+//             int eight = 8;
+//        System.out.println(eight);
+
+
+            // ================== IDENTIFIERS AND KEYWORDS (s-9)
+
+            // see curriculum
+
+
+
+            // ================== CONSTANTS (s-10)
+
+            // see curriculum
+
+//        final int SIDES_OF_A_DICE = 6;
+
+
+            // ================== EXPRESSIONS (s-11)
+
+            // basically the same as JS
+
+
+            // ================== OPERATORS (s-12)
+
+            // basically the same as JS
+
+
+            // ================== ASSIGNMENT STATEMENTS (s-13)
+
+            // basically the same as JS
+
+
+            // ================== CASTING (s-14)
+
+            // Link on casting... http://www.javainterviewpoint.com/type-casting-java-implicit-explicit-casting/
+
+
+            // IMPLICIT
+//            short x = 4;
+//            int y = x;
+//        System.out.println(x);
+//        System.out.println(y);
+
+
+            // EXPLICIT
+//            int a = 2000001;
+//            byte b = (byte) a;
+//        System.out.println(a);
+//        System.out.println(b);
+
+
+
+
+
+
+            // implicit casting - less to more precise (widening)
+
+//                 float num1 = 2.22F;
+//                 double num2 = num1;
+
+
+            // explicit casting - more to less explicit casting (narrowing)
+
+//                 double num3 = 3.1;
+//                 float num4 = (float) num3;
+//
+//                System.out.println(num4);
+
+
+
+        }
+
+    }
+
