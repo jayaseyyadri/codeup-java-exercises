5e9c3ca3878e240fd166359bb9900d4db808daa6
jaya <jayaseyyadri@gmail.com>
2020-12-03 14:26:32 -0600

    completed part b do-while loops

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,21 +1,27 @@
 public class ControlFlowExercises {
-    public static void main(String[]args){
-    int i =5;
-    while(i<=15){
-   System.out.printf(" %d ",i );
-   i+=1;
+    public static void main(String[] args) {
+/*  increment by two's  till 100*/
+//int i = 0;
+//do{
+//    System.out.println(i);
+//    i+=2;
+//}while(i<=100);
+
+        /*  decrease by 5 till -10  */
+//      int i =100;
+// do {
+//     System.out.println(i);
+//     i-=5;
+// }while (i>=-10);
+
+        /*  start at 2 till 1000000  */
+        float i= 2;
+do{
+    System.out.printf("%.0f%n",i);
+    i=(float)Math.pow(i,2.0);
+}while(i<=1000000);
+
     }
 
 
-
-
-
-
-
-
-
-
-
-
-    }
 }
