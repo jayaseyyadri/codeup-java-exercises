4772b4dbaee7bfa8712519d11ddf251deddb136b
jaya <jayaseyyadri@gmail.com>
2020-12-03 14:58:12 -0600

    finsihed refactoring with for

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,14 +1,14 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
-//    for(int i=5 ;i<=15;i++ ){
+//        for(int i=5 ;i<=15;i++ ){
 //        System.out.print(i+" " );
 //    }
 //        for(int i=0 ;i<=100;i=i+2 ){
 //        System.out.println(i);
 //    }
 
-        for(float i=2 ;i<1000000;i*=i ){
-        System.out.printf("%.0f%n", i );
-    }
+        for (float i = 2; i < 1000000; i *= i) {
+            System.out.printf("%.0f%n", i);
+        }
     }
 }
