e2cd1c264b1252d71bdc414a6d155a1a973a0677
jaya <jayaseyyadri@gmail.com>
2020-12-03 15:32:40 -0600

    check on for

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,18 +1,17 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
-//        for(int i=5 ;i<=15;i++ ){
-//        System.out.print(i+" " );
-//    }
-//        for(int i=0 ;i<=100;i=i+2 ){
-//        System.out.println(i);
-//    }
 
-        for (float i = 2; i < 1000000; i *= i) {
+        for (int i = 0; i <= 100; i++) {
 
-            System.out.printf("%.0f%n", i);
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("Fizzbuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 3 == 0 ) {
+                System.out.println("buzz");
+            } else{
+                    System.out.println(i);
+                }
+            }
         }
-
-
-
-    }
 }
