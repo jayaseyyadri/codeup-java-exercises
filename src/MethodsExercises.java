db6a236a1732c772a9ee1c49aa91036a243b297b
jaya <jayaseyyadri@gmail.com>
2020-12-04 15:50:47 -0600

    ex 1

---
+++ src/MethodsExercises.java
@@ -0,0 +1,33 @@
+public class MethodsExercises {
+    public static void main(String[] args) {
+//add(4 ,5);
+//sub(3,4);
+//divide(3,3);
+//multiply(1,5);
+//mod(5,4);
+//    }
+//    public static void add(int numberOne, int numberTwo){
+//        System.out.println( numberOne+numberTwo);
+//    }
+//    public static void sub(int numberOne, int numberTwo){
+//        System.out.println(numberOne-numberTwo);
+//    }
+//    public static void multiply(int numberOne, int numberTwo){
+//        System.out.println(numberOne*numberTwo);
+//    }
+//
+//    public static void divide(int numberOne, int numberTwo){
+//        System.out.println(numberOne/numberTwo);
+//    }
+//    public static void mod(int numberOne, int numberTwo){
+//        System.out.println(numberOne%numberTwo);
+
+table(3);
+
+    }
+public static void table(int number){
+for(int i=1;i<=10;i++){
+    System.out.format("%d * %d = %d \n",number,i,number*i);
+    }
+}
+}
