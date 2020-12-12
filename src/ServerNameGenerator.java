d0ea4fbc3e74d8cfde00967691034a2817cafd12
jaya <jayaseyyadri@gmail.com>
2020-12-11 20:55:45 -0600

    working on exercises

--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -3,7 +3,7 @@ public class ServerNameGenerator {
     private String[] adjectives = {"Pickled", "sweet", "Slippery", "Squeaking", "Ambidextrous", "Jazzy", "Loud", "Easy", "red", "chill"};
     private String[] nouns = {"paris", "coding", "cheese", "bread", "job", "puppy", "beach", "Cody", "book", "ball"};
 
-    public String nameGenerator(String[] adjectives, String[] nouns) {
+    public void nameGenerator(String[] adjectives, String[] nouns) {
              this.adjectives=adjectives;
              this.nouns=nouns;
     }
