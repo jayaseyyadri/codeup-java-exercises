3a98dfb268c61ca406471cace4237049a3bed977
jaya <jayaseyyadri@gmail.com>
2020-12-16 23:55:46 -0600

    exercises on ArrayList

---
+++ src/Student.java
@@ -0,0 +1,30 @@
+//import java.util.ArrayList;
+//
+//public class Student {
+//
+//    private String name;
+//    private ArrayList<Integer> grade = new ArrayList<>();
+//
+//public Student(String name, ArrayList<Integer> grade){
+//    this(name, grade;
+//    grade=new ArrayList<>();
+//}
+//
+//    public Student() {
+//
+//    }
+//
+//    // returns the student's name
+//    public String getName();
+//    // adds the given grade to the grades property
+//    public void addGrade(int grade);
+//
+//    // returns the average of the students grades
+//    public double getGradeAverage() {
+//        return GradeAverage/ ArrayList.;
+//    }
+//    public static void main(String[] srgs){
+//        Student a= new Student();
+//
+//    }
+//}
