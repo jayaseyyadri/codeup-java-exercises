fbb9a0b2ad6f840be449612b938a0bd3a30ed9f5
jaya <jayaseyyadri@gmail.com>
2020-12-23 17:02:51 -0600

    completed java fizzbuzz exercise

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -6,13 +6,23 @@ public class ControlFlowExercises {
 //        for(int i=0 ;i<=100;i=i+2 ){
 //        System.out.println(i);
 //    }
+//
+//        for (float i = 2; i < 1000000; i *= i) {
+//
+//            System.out.printf("%.0f%n", i);
+//        }
 
-        for (float i = 2; i < 1000000; i *= i) {
-
-            System.out.printf("%.0f%n", i);
+        //FizzBuzz
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
         }
-
-
-
     }
 }
